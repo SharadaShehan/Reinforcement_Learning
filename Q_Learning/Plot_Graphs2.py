19cb06b5e1d22064056bbfa2fa1fbf4b183416ca
Sharada Shehan <sharadashehan6@gmail.com>
2023-11-02 15:46:31 +0530

    Avearage Rewards graphs added

--- Q_Learning/Plot_Graphs2.py
+++ Q_Learning/Plot_Graphs2.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_graph(ax, title, learning_rate = 0.07, discount_rate = 0.8):
+def plot_graph(ax, title, learning_rate = 0.3, discount_rate = 0.95):
 
         # plot rewards
         rewards = train_model2(learning_rate,discount_rate)
         sns.scatterplot(x=range(len(rewards)), y=rewards, ax=ax, s=2, color='black', legend=False, linewidth = 0)
 
         # set up labels
-        ax.set_title(title, fontsize=12, y=0.7, pad=-14)
+        ax.set_title(title, fontsize=12, y=0.4, pad=-14)
         ax.set_xlabel('Episode', fontsize=10)
-        ax.set_ylabel('Reward', fontsize=10)
+        ax.set_ylabel('Average Reward', fontsize=10)
         ax.tick_params(axis='both', which='both', labelsize=6)
 
 def plot_alpha_graphs():
@@ -22,7 +22,7 @@ def plot_alpha_graphs():
         sns.set_style("darkgrid")
 
         mutiplication_factors = [1, 0.1, 0.01]
-        base_learning_rates = [0.1, 0.3, 0.5, 0.7]
+        base_learning_rates = [0.1, 0.5, 0.7, 0.9]
 
         fig, ax = plt.subplots(len(mutiplication_factors), len(base_learning_rates), figsize=(18,18))
         
@@ -31,7 +31,7 @@ def plot_alpha_graphs():
             for j in range(len(base_learning_rates)):
                 plot_graph(ax[i][j], f"\u03B1 = {round(base_learning_rates[j]*mutiplication_factors[i], 5)}", learning_rate = mutiplication_factors[i]*base_learning_rates[j])
 
-        plt.suptitle('Scatter plots of Reward Values against Number of Episodes for Different Learning Rates', fontsize=20)
+        plt.suptitle('Scatter plots of Average Reward Values against Number of Episodes for Different Learning Rates', fontsize=20)
 
         # show plots
         plt.tight_layout(pad=5.0)
@@ -51,7 +51,7 @@ def plot_gamma_graphs():
             for j in range(len(gammas[0])):
                 plot_graph(ax[i][j], f"\u03B3 = {round(gammas[i][j], 5)}", discount_rate = gammas[i][j])
         
-        plt.suptitle('Scatter plots of Reward Values against Number of Episodes for Different Discount Rates', fontsize=20)
+        plt.suptitle('Scatter plots of Average Reward Values against Number of Episodes for Different Discount Rates', fontsize=20)
         
         # show plots
         plt.tight_layout(pad=4.0)
@@ -64,7 +64,7 @@ def plot_alpha_against_gamma_graphs():
         sns.set_style("darkgrid")
         
         learning_rates = [0.5, 0.05, 0.005]
-        gammas = [0.8, 0.9, 0.99, 0.999]
+        gammas = [0.4, 0.7, 0.9, 0.999]
         
         fig, ax = plt.subplots(len(learning_rates), len(gammas), figsize=(18,18))
 
@@ -72,7 +72,7 @@ def plot_alpha_against_gamma_graphs():
             for j in range(len(gammas)):
                 plot_graph(ax[i][j], f"\u03B1 = {round(learning_rates[i], 5)}  \u03B3 = {round(gammas[j], 5)}", learning_rate = learning_rates[i], discount_rate = gammas[j])
         
-        plt.suptitle('Scatter plots of Reward Values against Number of Episodes for Different Learning Rates and Discount Rates', fontsize=20)
+        plt.suptitle('Scatter plots of Average Reward Values against Number of Episodes for Different Learning Rates and Discount Rates', fontsize=20)
         
         # show plots
         plt.tight_layout(pad=5.0)
--- Q_Learning/Train_Model.py
+++ Q_Learning/Train_Model.py
@@ -3,7 +3,7 @@ import gym
 import random
 
 
-def train_model(learning_rate = 0.9, discount_rate = 0.8):
+def train_model2(learning_rate = 0.9, discount_rate = 0.8):
 
     # create Taxi environment
     env = gym.make('Taxi-v3', render_mode='ansi')
@@ -21,7 +21,7 @@ def train_model(learning_rate = 0.9, discount_rate = 0.8):
     num_episodes = 1000
     max_steps = 99 # per episode
 
-    final_rewards = []
+    average_rewards = []
 
     # training
     for episode in range(num_episodes):
@@ -56,9 +56,72 @@ def train_model(learning_rate = 0.9, discount_rate = 0.8):
         # Decrease epsilon
         epsilon = np.exp(-decay_rate*episode)
 
-        final_rewards.append(reward)
+        average_rewards.append(reward)
 
     env.close()
 
-    return final_rewards
+    return average_rewards
+
+
+
+def train_model2(learning_rate = 0.9, discount_rate = 0.8):
+
+    # create Taxi environment
+    env = gym.make('Taxi-v3', render_mode='ansi')
+
+    # initialize q-table
+    state_size = env.observation_space.n
+    action_size = env.action_space.n
+    qtable = np.zeros((state_size, action_size))
+
+    # hyperparameters
+    epsilon = 1.0
+    decay_rate= 0.005
+
+    # training variables
+    num_episodes = 1000
+    max_steps = 99 # per episode
+
+    average_rewards = []
+
+    # training
+    for episode in range(num_episodes):
+
+        # reset the environment
+        state = env.reset()
+        done = False
+        sum_rewards = 0
+
+        for s in range(max_steps):
+
+            # exploration-exploitation tradeoff
+            if random.uniform(0,1) < epsilon:
+                # explore
+                action = env.action_space.sample()
+            else:
+                # exploit
+                action = np.argmax(qtable[state,:])
+
+            # take action and observe reward
+            new_state, reward, done, info = env.step(action)
+            sum_rewards += reward
+
+            # Q-learning algorithm
+            qtable[state,action] = qtable[state,action] + learning_rate * (reward + discount_rate * np.max(qtable[new_state,:])-qtable[state,action])
+
+            # Update to our new state
+            state = new_state
+
+            # if done, finish episode
+            if done == True:
+                break
+
+        # Decrease epsilon
+        epsilon = np.exp(-decay_rate*episode)
+
+        average_rewards.append(sum_rewards/max_steps)
+
+    env.close()
+
+    return average_rewards
 
